d825a7eb4f25721f7be9020f0a65636c2ccbc304
Sergey Bochenkov <bachan@j3qq4.org>
2011-04-07 22:48:23 +0400

    codestyle

--- blizzard/utils.hpp
+++ blizzard/utils.hpp
@@ -1,12 +1,8 @@
-#ifndef __LIZARD_UTILS_HPP___
-#define __LIZARD_UTILS_HPP___
+#ifndef __BLIZZARD_UTILS_HPP__
+#define __BLIZZARD_UTILS_HPP__
 
 #include <stdint.h>
 #include <coda/logger.h>
-
-/* namespace blizzard { */
-/* extern int MSG_LIZARD_ID; */
-/* } */
 
 namespace lz_utils {
 
@@ -20,10 +16,6 @@ int add_listener(const char * host_desc, const char * port_desc, int listen_q_sz
 int add_sender(const char * host_desc, const char * port_desc);
 int accept_new_connection(int fd, struct in_addr& ip);
 
-/* bool pid_file_init(const char * pid_fn); */
-/* bool pid_file_check(const char * pid_fn); */
-/* bool pid_file_free(const char * pid_fn); */
-
 }
 
-#endif
+#endif /* __BLIZZARD_UTILS_HPP__ */
